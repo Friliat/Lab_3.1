8bef43fe2adbaffdc3fc4b24374c98b84eedb8e4
Friliat <rz389905@gmail.com>
2024-02-12 09:46:58 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             double F(double x, int b = 8)
+            {
+                if (b == 0)
+                {
+                    return 2;
+                }
+                else
+                    return b * Math.Cos((b - 1) * x) + F(x, --b);
+            }
+
+            double X = double.Parse(Console.ReadLine());
+            Console.WriteLine(F(X));
+        }
+    }
+}
